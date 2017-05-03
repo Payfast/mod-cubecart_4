140a343e9c25c1e711b726583b0dc3f7257039a8
Brendon Posen <brendon.posen@payfast.co.za>
2017-05-03 13:36:18 +0200

    update

--- modules/gateway/PayFast/admin/index.inc.php
+++ modules/gateway/PayFast/admin/index.inc.php
@@ -3,8 +3,9 @@ if( !defined( 'CC_INI_SET' ) ) die( "Access Denied" );
 /**
  * index.inc.php
  *
- * @copyright Copyright 2008, PayFast (Pty) Ltd
- * @copyright Portions Copyright Devellion Limited 2006
+ * Copyright (c) 2008 PayFast (Pty) Ltd
+ * You (being anyone who is not PayFast (Pty) Ltd) may download and use this plugin / code in your own website in conjunction with a registered and active PayFast account. If your PayFast account is terminated for any reason, you may not use this plugin / code or part thereof.
+ * Except as expressly indicated in this licence, you may not use, copy, modify or distribute this plugin / code or part thereof in any way.
  * @author Jonathan Smit
  */
 
